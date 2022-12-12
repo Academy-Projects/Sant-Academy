92aa6019841da5fc487ca47eabc60eb03473f854
kevin marinho <kevindmarinho4321@gmail.com>
2022-12-12 14:47:19 -0400

    Initial Commit

---
+++ Sant Academy/Sant_AcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sant_AcademyApp.swift
+//  Sant Academy
+//
+//  Created by kevin marinho on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Sant_AcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
