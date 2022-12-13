989d90a136abc38a405232c5fd5bbe9631a8b279
kevin marinho <kevindmarinho4321@gmail.com>
2022-12-12 22:10:54 -0400

    tentando adicionar a camera

---
+++ Sant Academy/Model/CameraModel.swift
@@ -0,0 +1,86 @@
+//
+//  CameraModel.swift
+//  Sant Academy
+//
+//  Created by kevin marinho on 12/12/22.
+//
+
+import Foundation
+import SwiftUI
+import AVFoundation
+
+class CameraModel: ObservableObject{
+    @Published var isTaken = false
+    
+    @Published var session = AVCaptureSession()
+    
+    @Published var alert = false
+    
+    @Published var output = AVCapturePhotoOutput()
+    
+    @Published var preview : AVCaptureVideoPreviewLayer!
+    
+    func Check(){
+            switch AVCaptureDevice.authorizationStatus(for: .video){
+            case .authorized:
+                setUp()
+                return
+                
+            case .notDetermined:
+                AVCaptureDevice.requestAccess(for: .video){(status) in
+                    if status{
+                        self.setUp()
+                    }
+                }
+            case .denied:
+                self.alert.toggle()
+                return
+            default:
+                return
+            }
+    }
+    
+    func setUp(){
+        do{
+            self.session.beginConfiguration()
+            
+            let device = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back)
+            
+            let input = try AVCaptureDeviceInput(device: device!)
+            
+            if self.session.canAddInput(input){
+                self.session.addInput(input)
+            }
+            
+            if self.session.canAddOutput(self.output){
+                self.session.addOutput(self.output)
+            }
+            self.session.commitConfiguration()
+        }
+        catch{
+            print(error.localizedDescription)
+        }
+    }
+}
+
+struct CameraPreview: UIViewRepresentable{
+    
+    @ObservedObject var camera : CameraModel
+        func makeUIView(context: Context) ->  UIView {
+            let view = UIView(frame: UIScreen.main.bounds)
+            
+            camera.preview = AVCaptureVideoPreviewLayer(session: camera.session)
+            camera.preview.frame = view.frame
+            
+            camera.preview.videoGravity = .resizeAspectFill
+            view.layer.addSublayer(camera.preview)
+            
+            camera.session.startRunning()
+            
+            return view
+        }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        
+    }
+}
