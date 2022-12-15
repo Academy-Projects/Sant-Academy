81b2dfa339cf4b6d944c315f3992cd45eb928a89
Leticia <leticiavictoria99@gmail.com>
2022-12-15 03:26:42 -0400

    criando MasterView

--- Sant Academy/Model/Sant_AcademyApp.swift
+++ Sant Academy/Model/Sant_AcademyApp.swift
@@ -9,9 +9,24 @@ import SwiftUI
 
 @main
 struct Sant_AcademyApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate .self) var appDelegate
+   
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView{
+                MasterView()
+            }
+           
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    static var orientationLock = UIInterfaceOrientationMask.all
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
     }
 }
